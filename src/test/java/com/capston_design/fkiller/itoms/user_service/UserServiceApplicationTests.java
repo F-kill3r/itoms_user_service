42604e604f2cda1c8769c233f1d3ff4133e19b95
박성준 <sungjunp1208@gmail.com>
2025-05-23 12:42:38 +0900

    init: user service 초기 세팅

---
+++ src/test/java/com/capston_design/fkiller/itoms/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capston_design.fkiller.itoms.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
