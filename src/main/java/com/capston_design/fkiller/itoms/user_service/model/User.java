046f7bcf6614fb1f605b3a3a33da2ec0a7dbcdac
박성준 <sungjunp1208@gmail.com>
2025-05-23 20:33:54 +0900

    fix: change User Entity id long->UUID

--- src/main/java/com/capston_design/fkiller/itoms/user_service/model/User.java
+++ src/main/java/com/capston_design/fkiller/itoms/user_service/model/User.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 
 @Entity
 @Table(name= "t_user")
@@ -18,7 +20,7 @@ import lombok.Setter;
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
     private String name;
 
     @Enumerated(EnumType.STRING)
