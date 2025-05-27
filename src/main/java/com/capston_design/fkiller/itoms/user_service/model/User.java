c6db8f7fc7dc29437445b9e928a2dff109fd5fb3
박성준 <sungjunp1208@gmail.com>
2025-05-27 14:30:37 +0900

    fix: assign UUID automatically using @PrePersist

--- src/main/java/com/capston_design/fkiller/itoms/user_service/model/User.java
+++ src/main/java/com/capston_design/fkiller/itoms/user_service/model/User.java
@@ -19,10 +19,16 @@ import java.util.UUID;
 @NoArgsConstructor
 public class User extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
     private String name;
 
     @Enumerated(EnumType.STRING)
     private UserCategory category;
+
+    @PrePersist
+    public void assignUUID() {
+        if (this.id == null) {
+            this.id = UUID.randomUUID();
+        }
+    }
 }
